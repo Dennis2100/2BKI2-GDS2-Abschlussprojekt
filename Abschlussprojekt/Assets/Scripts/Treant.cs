992771b099bec764a6a549a9199c45f2f4afc952
Timo <timo.binder.01@gmail.com>
2022-01-23 19:04:05 +0100

    Anfang der Animationen für die Gegner und die Gestaltung der ersten Bereiche der Map

--- Abschlussprojekt/Assets/Scripts/Treant.cs
+++ Abschlussprojekt/Assets/Scripts/Treant.cs
@@ -9,12 +9,15 @@ public class Treant : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
         currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
@@ -32,9 +35,42 @@ public class Treant : Enemy
             if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-                myRigidbody.MovePosition(temp);
 
+                changeAnim(temp - transform.position);
+                myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+            }
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    private void changeAnim(Vector2 direction)
+    {
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if(direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if(direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if(direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if(direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
