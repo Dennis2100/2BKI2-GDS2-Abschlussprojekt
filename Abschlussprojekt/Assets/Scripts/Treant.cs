6d4ef81fb9ab0828bd17cfd6a956aec4522440a8
DESKTOP-MUC9CDK\Dennis <melnychuk.dennis@gmx.de>
2022-03-10 19:26:01 +0100

    Bugfix der Treants

--- Abschlussprojekt/Assets/Scripts/Treant.cs
+++ Abschlussprojekt/Assets/Scripts/Treant.cs
@@ -39,7 +39,12 @@ public class Treant : Enemy
                 changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("moving", true);
             }
+        }
+        else
+        {
+            anim.SetBool("moving", false);
         }
     }
 
